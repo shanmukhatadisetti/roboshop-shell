5ffc69a2bbae5bd77b46bcea043961c86cf805ec
shanmukh <shanmukhatadisetti@gmail.com>
2023-02-19 17:02:38 +0530

    catalogue.services

--- catalogue.sh
+++ catalogue.sh
@@ -8,3 +8,10 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app
 npm install
+cp configs/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
