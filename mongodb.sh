c1e9c93dcaeec961a58a00b5d6c84c8a25791164
shanmukh <shanmukhatadisetti@gmail.com>
2023-02-20 23:40:19 +0530

    added redirectories and functions to mongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,21 @@
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+
+print_head() {
+  echo -e "\e[35m$1\e[0m"
+}
+
+print_head "Setup MongoDB repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install MongoDB"
 yum install mongodb-org -y
+
+print_head "Enable MongoDB"
 systemctl enable mongod
+
+print_head "start MongoDB"
 systemctl start mongod
+
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
