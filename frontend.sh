c6dd12eb648f5a3570472484d5bd422536205b66
shanmukh <shanmukhatadisetti@gmail.com>
2023-02-20 22:45:18 +0530

    adding colour and description to code in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,16 @@
 code_dir=$(pwd)
+echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
+echo -e "\e[35mRemoving Old Content\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[35mDownloading Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[35mExtracting Downloaded Frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+echo -e "\e[35mCopying Nginx Config for Roboshop\e[0m"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[35mEnableing nginx\e[0m"
 systemctl enable nginx
+echo -e "\e[35mStarting nginx\e[0m"
 systemctl start nginx
