25ae6d54286f66f4bf9af787278d73502ae99961
shanmukh <shanmukhatadisetti@gmail.com>
2023-02-22 20:25:00 +0530

    catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -2,47 +2,62 @@ source common.sh
 
 print_head "Configure NodeJS Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+echo $?
 
 print_head "Install NodeJS"
 yum install nodejs -y &>>${log_file}
+echo $?
 
 print_head "Create Robshop User"
 useradd roboshop &>>${log_file}
+echo $?
 
 print_head "Create Application Directory"
 mkdir /app &>>${log_file}
+echo $?
 
 print_head "Delete Old Content"
 rm -rf /app/* &>>${log_file}
+echo $?
 
 
 print_head "Downloading App Content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
+echo $?
 
 print_head "Extracting App Content"
 unzip /tmp/catalogue.zip &>>${log_file}
+echo $?
 
 print_head "Installing NodeJS Dependencies"
 npm install &>>${log_file}
+echo $?
 
 print_head "copy SystemD Service File"
 cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+echo $?
 
 print_head "Reload SystemD"
 systemctl daemon-reload &>>${log_file}
+echo $?
 
 print_head "Enable Catalogue Service"
 systemctl enable catalogue &>>${log_file}
+echo $?
 
 print_head "Start Catalogue Service "
 systemctl start catalogue &>>${log_file}
+echo $?
 
 print_head "Copy MongoDB Repo File"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+echo $?
 
 print_head "Install MongoDB Client"
 yum install mongodb-org-shell -y &>>${log_file}
+echo $?
 
 print_head "Load Schema"
 mongo --host mongodb.autonagar.in </app/schema/catalogue.js &>>${log_file}
+echo $?
