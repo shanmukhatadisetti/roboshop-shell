e691762900381af9c34c77691878b94aa5ff9cb7
shanmukh <shanmukhatadisetti@gmail.com>
2023-02-22 20:40:19 +0530

    mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -2,21 +2,21 @@ source common.sh
 
 print_head "Setup MongoDB repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Install MongoDB"
 yum install mongodb-org -y &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Update MongoDB Listen address"
 sed -i -r 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
-echo $?
+status_check $?
 
 print_head "start MongoDB"
 systemctl restart mongod &>>${log_file}
-echo $?
+status_check $?
 
