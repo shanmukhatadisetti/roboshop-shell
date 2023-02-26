14a608ae92d42642ddc7bd5a6dbab77236f7d017
shanmukh <shanmukhatadisetti@gmail.com>
2023-02-26 20:32:51 +0530

    mysql.repo

---
+++ mysql.sh
@@ -0,0 +1,7 @@
+source common.sh
+
+dnf module disable mysql -y
+yum install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass RoboShop@1
