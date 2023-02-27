b9a6580839a5beeaea96442db017c3c81ef859b1
shanmukh <shanmukhatadisetti@gmail.com>
2023-02-27 21:07:58 +0530

    shipping.service

--- common.sh
+++ common.sh
@@ -93,5 +93,11 @@ nodejs() {
 }
 java() {
 
+yum install maven -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+cd /app
+unzip /tmp/shipping.zip
 
 }
