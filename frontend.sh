19b3e5a3c345279e8d6c3863a9497e2bc75403be
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-20 16:48:50 +0000

    shell-scripting

--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,7 @@ W="\e[0m"
 
 SCRIPT_PATH=/var/log/roboshop_log
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$SCRIPT_PATH/$SCRIPT_NAME"
+LOG_FILE="$SCRIPT_PATH/$SCRIPT_NAME.log"
 user_home=$PWD
 mkdir -p $SCRIPT_PATH
 
@@ -17,18 +17,18 @@ echo "Script executed on:: $(date)"
 
 if [ $user_id == 0 ]
 then
-    echo -e "$Y Your Running with Root User $W" | tee -a &>>$LOG_FILE
+    echo -e "$Y Your Running with Root User $W" | tee -a $LOG_FILE
 else
-    echo -e "$R ERROR:: Your Not Running with Root User $W" | tee -a &>>$LOG_FILE
+    echo -e "$R ERROR:: Your Not Running with Root User $W" | tee -a $LOG_FILE
     exit 1
 fi
 
 VALIDATION(){
     if [ $1 == 0 ]
     then 
-        echo -e "$G $2 is Successful $W" | tee -a &>>$LOG_FILE
+        echo -e "$G $2 is Successful $W" | tee -a $LOG_FILE
     else
-        echo -e "$R $2 is Failed $W" | tee -a &>>$LOG_FILE
+        echo -e "$R $2 is Failed $W" | tee -a $LOG_FILE
     fi
 }
 
