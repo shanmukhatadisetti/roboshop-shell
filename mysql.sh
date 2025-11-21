0ec7eb0a7e123409aa940f0d469560a16726d288
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-21 16:17:52 +0000

    shell-scripting

---
+++ mysql.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+SCRIPT_START=$(date +%s)
+user_id=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+SCRIPT_PATH=/var/log/roboshop_log
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$SCRIPT_PATH/$SCRIPT_NAME.log"
+user_home=$PWD
+mkdir -p $SCRIPT_PATH
+
+echo "Script executed on:: $(date)"
+
+echo -e "Enter MYSQL PASSWORD"
+read -s MYSQL_PASSWORD
+
+if [ $user_id == 0 ]
+then
+    echo -e "$Y Your Running with Root User $W" | tee -a $LOG_FILE
+else
+    echo -e "$R ERROR:: Your Not Running with Root User $W" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATION(){
+    if [ $1 == 0 ]
+    then 
+        echo -e "$G $2 is Successful $W" | tee -a $LOG_FILE
+    else
+        echo -e "$R $2 is Failed $W" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf install mysql-server -y
+VALIDATION $? "Installing Mysql Server"
+
+systemctl enable mysqld
+VALIDATION $? "Enabling Mysql service"
+
+systemctl start mysqld
+VALIDATION $? "Starting Mysql service"
+
+mysql_secure_installation --set-root-pass $MYSQL_PASSWORD
+VALIDATION $? "Setting up Mysql password"
+
+SCRIPT_END=$(date +%s)
+
+SCRIPT_TOTAL_TIME=$(( $SCRIPT_END - $SCRIPT_START ))
+
+echo -e "Total Execution Time For the Script To Run:: $Y $SCRIPT_TOTAL_TIME seconds $W" | tee -a $LOG_FILE
