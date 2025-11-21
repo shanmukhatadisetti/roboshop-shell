b4557f3cc81cd073670bfdddc69a1e81407f6df3
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-21 13:29:27 +0000

    shell-scripting

--- redis.sh
+++ redis.sh
@@ -52,4 +52,4 @@ SCRIPT_END=$(date +%s)
 
 SCRIPT_TOTAL_TIME=$(( $SCRIPT_END - $SCRIPT_START ))
 
-echo -e "Total Execution Time For the Script To Run:: $Y $SCRIPT_TOTAL_TIME $W" | tee -a $LOG_FILE
+echo -e "Total Execution Time For the Script To Run:: $Y $SCRIPT_TOTAL_TIME-seconds $W" | tee -a $LOG_FILE
