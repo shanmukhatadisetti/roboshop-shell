6bb88d3a2a24237832f1e42b5892d035ff44f6ac
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-11-21 00:44:05 +0000

    shell-scripting

---
+++ user.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+W="\e[0m"
+
+
+
+SCRIPT_PATH=/var/log/roboshop_logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$SCRIPT_PATH/$SCRIPT_NAME.log"
+
+user_home=$PWD
+
+mkdir -p $SCRIPT_PATH
+
+echo -e "Script started on $(date)"
+
+if [ $userid == 0 ]
+then
+    echo -e "$Y Your Running with root user $W" | tee -a $LOG_FILE
+else
+    echo -e "$R ERROR:: Your not running with root user $W" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATION(){
+    if [ $1 == 0 ]
+    then
+        echo -e "$G $2  is Successful $W" | tee -a $LOG_FILE
+    else
+        echo -e "$R ERROR:: $2  is failed $W" | tee -a $LOG_FILE
+    fi 
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATION $? "Disabling Nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATION $? "Enabled Nodejs 20 Version"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATION $? "Installing Nodejs "
+
+id roboshop &>>$LOG_FILE
+if [ $? != 0 ]
+then 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATION $? "Creating System User"
+else
+    echo -e "System User already Created...$Y Skipping $W"
+fi
+
+rm -rf /app/*
+
+mkdir -p /app &>>$LOG_FILE
+VALIDATION $? "Created app Directory"
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATION $? "Dowloading App Content"
+
+cd /app
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATION $? "Changed to /app and Unzipping Content"
+
+npm install &>>$LOG_FILE
+VALIDATION $? "Installing Dependencies"
+
+cp $user_home/user.service /etc/systemd/system/user.service &>>$LOG_FILE
+VALIDATION $? "Copying user Service File"
+
+systemctl daemon-reload
+systemctl enable user 
+systemctl restart user
+VALIDATION $? "Starting user Service"
